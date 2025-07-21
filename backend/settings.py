551ebfa58b1e6bf0452222034c270ff5ad15ba37
Emmanuelobakpolor <emmanuelobakpolor6655@gmail.com>
2025-07-21 22:41:46 +0100

    c

--- backend/settings.py
+++ backend/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "https://vaultifyadmin.onrender.com",  # 👈 Your frontend URL
 ]
