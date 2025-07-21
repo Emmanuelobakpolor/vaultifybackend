d848b0fec29434d4797962b1d38748be81d59191
Emmanuelobakpolor <emmanuelobakpolor6655@gmail.com>
2025-07-21 22:07:04 +0100

    C

--- backend/settings.py
+++ backend/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
 
 
 CORS_ALLOWED_ORIGINS = [
-    'https://vaultifyadmin.onrender.com',
+    'https://vaultifyadmin.onrender.com', 'https://vaultifybackend.onrender.com', '*'
 ]
 CORS_ALLOW_CREDENTIALS = True
 
